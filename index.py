f401b5d834edc575082c295f4b8cfd5dd6871b03
areesha <164901125+Areeshsheikh@users.noreply.github.com>
2025-03-02 19:17:27 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,39 @@
+print("Hello, World!")
+
+# -------------------------------
+
+# Pycache Example
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Areesha"))
+
+# -------------------------------
+
+# data types
+
+# string
+name = "Areesha"
+
+# integer
+age = 21
+
+# float
+height = 5.7
+
+# boolean
+is_student = True
+
+# list
+grades = [95, 83, 90, 87, 74]
+
+# tuple
+coordinates = (3, 5)
+
+# dictionary
+person = {"name": "Areesha", "age": 18, "height": 5.2}
+
+# set
+unique_numbers = {1, 2, 3, 4, 5}
+
+print(name, age, height, is_student, grades, coordinates, person, unique_numbers)
